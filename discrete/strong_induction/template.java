a439793980d7fa0e9299c1f551b5c4bafc21c876
Suren Poghosyan <suren.poghosian.18@gmail.com>
2025-09-15 11:59:55 +0400

    initiate

---
+++ discrete/strong_induction/template.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  static void sample_function(int number) {
+    System.out.print("Sample function called - ");
+    System.out.println(number);
+  }
+
+  public static void main(String[] args) {
+    sample_function(1);
+  }
+}
